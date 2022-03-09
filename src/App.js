a6bddba75a8cf3ef33a46b93697b6176a9aad830
Bill Gathen <bill@billgathen.com>
2022-03-09 08:24:49 -0500

    Basic page layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pentaphone</h1>
+    <div className="app">
+      <div className="header">
+        <h1>Pentaphone</h1>
+      </div>
+      <div className="main-interface">
+        <div>Keyboard will go here</div>
+      </div>
+      <div className="footer">
+        <div>Created by Bill Gathen</div>
+        <div>
+          <a class="link" href="http://billgathen.com">
+            Website
+          </a>
+        </div>
+        <div>
+          <a class="link" href="http://github.com/billgathen">
+            GitHub
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
